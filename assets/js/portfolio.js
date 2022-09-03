9e65db376d89e2e6ca4451f76c45b9f88e30a443
Fayas P A <fayasmuthalib@gmail.com>
2022-09-03 19:24:54 +0530

    new updation 2

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -11,13 +11,13 @@ var submitError = document.getElementById('submit-error');
 function validateName() {
     var name=document.getElementById('name').value.trim();  
 
-    if (name.length==0) {
-        nameError.innerHTML='Name is Required';
+    if (name.length==0 || name.length<3) {
+        nameError.innerHTML='Enter min 3 characters';
         nameError.style.color='red'
         return false;
     }
 
-    if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+    if (!name.match(/^[A-Za-z' ']*$/)){
         nameError.innerHTML='Write a FullName';
         nameError.style.color='red'
         return false;
@@ -45,19 +45,51 @@ function validateEmail(){
     emailError.style.color='green'
     return true;
 }
-function validateMessage() {
+
+function validateMsg() {
     var message =document.getElementById('message').value.trim();
-    var Required=15;
-    var left=Required - message.length;
     
-    if(left>0){
-        messageError.innerHTML =left+ 'more character Required';
+    if(message.length==0 || message.length<10){
+        messageError.innerHTML ='more character Required';
         messageError.style.color='red'
         return false;
-    
     }
     messageError.innerHTML='Message is valid';
     messageError.style.color='green'
     return true;   
 }
+function validateTel() {
+    var Tel =document.getElementById('telephone').value.trim();
+        if(Tel.match(/^[0-9]+$/) && Tel.length>10)
+        {
+            telError.innerHTML='Message is valid';
+            telError.style.color='green'
+            return true;
+        }
+
+        else{
+            telError.innerHTML ='Mobile Number is Required';
+            telError.style.color='red'
+            return false;
+        }
+    }
+    function validateSubject() {
+        var subject=document.getElementById('subject').value.trim();  
+    
+        if (subject.length==0) {
+            subjectError.innerHTML='This field is Required';
+            subjectError.style.color='red'
+            return false;
+        }
+    
+        if (!subject.match(/^[A-Za-z ]*$/)){
+            subjectError.innerHTML='Write a FullName';
+            subjectError.style.color='red'
+            return false;
+        }
+        
+        subjectError.innerHTML='Name is valid';
+        subjectError.style.color='green'
+        return true;
+    }
 
